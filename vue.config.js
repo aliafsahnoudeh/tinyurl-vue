e875f49058529d16bd68386e77bbabb87f96bf9f
Ali <aliafsah1988@gmail.com>
2020-01-19 22:53:37 +0330

    'deploy'

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+// const path = require("path");
+// const PrerenderSPAPlugin = require("prerender-spa-plugin");
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/tinyurl-vue/" : "/"
+  //   configureWebpack: config => {
+  //     if (process.env.NODE_ENV !== "production") return;
+
+  //     return {
+  //       plugins: [
+  //         new PrerenderSPAPlugin({
+  //           staticDir: path.join(__dirname, "docs"),
+  //           routes: ["/", "/:linkId"],
+  //           minify: {
+  //             collapseBooleanAttributes: true,
+  //             collapseWhitespace: true,
+  //             decodeEntities: true,
+  //             keepClosingSlash: true,
+  //             sortAttributes: true
+  //           },
+  //           renderer: new Renderer({ headless: true })
+  //         })
+  //       ]
+  //     };
+  //   }
+};
